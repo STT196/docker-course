3d769aee556034d315411773ab2722ae8fad2b9b
STT196 <109481005+STT196@users.noreply.github.com>
2024-04-20 23:19:35 +0530

    Update Dockerfile and cicd.yml to expose port 5174 instead of 5173

--- react-docker/vite.config.js
+++ react-docker/vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-    server: {
-    //   host: "172.25.82.128",
-      port:4000,
-    }
+    // server: {
+    // //   host: "172.25.82.128",
+    //   port:4000,
+    // }
 })
